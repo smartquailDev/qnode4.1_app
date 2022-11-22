151309f426267f514a6c09d668865d51280b1198
smartquail <smartquail@MacBook-Air-de-smartquail.local>
2022-11-21 20:47:23 -0500

    test

--- qnode41_app/qnode41_app/settings/base.py
+++ qnode41_app/qnode41_app/settings/base.py
@@ -6,6 +6,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DJANGO_SECRET_KEY= os.environ.get('DJANGO_SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
